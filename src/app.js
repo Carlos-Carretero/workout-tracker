572ceaed9f9d5d43f0d1972717e2dc3e1371e287
Carlos Carretero <carreteroestudios@gmail.com>
2025-04-02 20:26:47 -0500

    Configuracion variables de entorno en el proyecto

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
-const express = require("express"); // Import express
-const app = express(); // Create an instance of express
-const port = 8000; // puerto de escucha
+const express = require("express");
+const { port } = require('./config/env'); 
+const app = express();
 
-// Inicializacion del servidor y primera ruta
 app.get("/", (req, res) => {
   res.send("Hola mi server en Express 💩");
 });
