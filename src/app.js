ecd563eec79133cec405c40fad9e94cc14cc582e
Carlos Carretero <carreteroestudios@gmail.com>
2025-04-02 20:15:11 -0500

    configuracion del servidor web en express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("Aplicación trabajando en el proyecto");
+const express = require("express"); // Import express
+const app = express(); // Create an instance of express
+const port = 8000; // puerto de escucha
+
+// Inicializacion del servidor y primera ruta
+app.get("/", (req, res) => {
+  res.send("Hola mi server en Express 💩");
+});
+
+// Inicio del servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
